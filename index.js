6ee3558264699bf02043feb164778292f5fcc8cd
Camily023 <113644994+Camily023@users.noreply.github.com>
2022-09-15 20:07:46 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/',(req, res)=>{
+    res.send('Funcionouuuuuuuuuuu!!!')
+});
+
+app.listen(3000,()=>{
+    console.log('servidor rodando')
+});
